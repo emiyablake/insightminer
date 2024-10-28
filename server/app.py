e1efc70974cd691a590bf76fa94bbfeb33a23046
Maju <127452115+emiyablake@users.noreply.github.com>
2024-10-28 14:51:00 -0300

    Revert "Create pages and routes"

--- server/app.py
+++ server/app.py
@@ -1,20 +1,8 @@
-# from fastapi import FastAPI
-#
-# app = FastAPI()
-#
-#
-# @app.get("/")
-# async def root():
-#     return {"message": "Hello World"}
-
-
-
 from fastapi import FastAPI
-from server.roots import root_news
-# from roots import root_news
+
 app = FastAPI()
 
 
-app.include_router(root_news.router, prefix="/api")
-
-# print('ok')
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}

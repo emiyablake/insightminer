ed3eac8252c975e4c1b95d1ec54b7b7a957cb82d
Gabriel Ratao <gabiratao28@gmail.com>
2024-11-20 21:06:22 -0300

    add cors config and txt with comand docker lines

--- server/app.py
+++ server/app.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from roots import root_news
 
 app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=['*']
+)
+
 app.include_router(root_news.router, prefix="/api")

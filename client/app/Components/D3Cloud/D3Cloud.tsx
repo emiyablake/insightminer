3ef620a206acc49d40c66a05d665c58f578e4ff2
emiyablake <maju.ads.02@gmail.com>
2024-11-23 13:48:06 -0300

    create comp and page

---
+++ client/app/Components/D3Cloud/D3Cloud.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import * as d3 from 'd3';
+import cloud from 'd3-cloud';
+import { useEffect, useRef } from 'react';
+
+interface Word {
+  text: string;
+  size: number;
+}
+
+const customColors = [
+  "#FF5733", "#33FF57", "#3357FF", "#FF33A6", "#FF8C00",
+  "#8CFF00", "#00FF8C", "#FF0033", "#FF57FF", "#57FF33"
+];
+
+const D3Cloud = ({ words }: { words: Word[] }) => {
+  const svgRef = useRef<SVGSVGElement>(null);
+
+  useEffect(() => {
+    const svgElement = svgRef.current;
+    if (!svgElement) return; // Verifique se svgRef.current não é nulo
+
+    const svg = d3.select(svgElement);
+    const width = 500;
+    const height = 300;
+
+    // Limpa renderizações anteriores
+    svg.selectAll('*').remove();
+
+    const layout = cloud()
+      .size([width, height])
+      .words(
+        words.map((word) => ({
+          text: word.text,
+          size: word.size,
+        }))
+      )
+      .padding(5)
+      .rotate(() => (Math.random() > 0.5 ? 90 : 0))
+      .fontSize((d: any) => (d.size ? d.size : 10))
+      .on('end', (renderedWords) => {
+        // Verifique se a referência do SVG está disponível novamente antes de tentar manipulá-lo
+        if (!svgRef.current) return;
+
+        svg
+          .attr('width', width)
+          .attr('height', height)
+          .append('g')
+          .attr('transform', `translate(${width / 2}, ${height / 2})`)
+          .selectAll('text')
+          .data(renderedWords)
+          .enter()
+          .append('text')
+          .style('font-size', (d: any) => `${d.size}px`)
+          .style('fill', (d: any, i: number) => customColors[i % 10])
+          .attr('text-anchor', 'middle')
+          .attr('transform', (d: any) => `translate(${d.x}, ${d.y}) rotate(${d.rotate})`)
+          .text((d: any) => d.text);
+      });
+
+    layout.start();
+  }, [words]); // Dependência do "words" para atualizar sempre que o valor de "words" mudar
+
+  return <svg ref={svgRef}></svg>;
+};
+
+export default D3Cloud;
--- client/app/wordcloud/page.tsx
+++ client/app/wordcloud/page.tsx
@@ -3,15 +3,48 @@ import { NextPage } from "next";
 import Header from "../Components/Header/Header"
 import Footer from "../Components/Footer/Footer"
 import styles from "./wordcloud.module.css"
+import D3Cloud from "../Components/D3Cloud/D3Cloud";
 
 
 const WordCloud: NextPage = () => {
+    const words = [
+        { text: "Next.js", size: 30 },
+        { text: "React", size: 50 },
+        { text: "JavaScript", size: 40 },
+        { text: "Web", size: 25 },
+        { text: "D3", size: 35 },
+        { text: "Cloud", size: 45 },
+        { text: "Visualization", size: 20 },
+        { text: "Frontend", size: 30 },
+        { text: "Next.js", size: 30 },
+        { text: "React", size: 50 },
+        { text: "JavaScript", size: 40 },
+        { text: "Web", size: 25 },
+        { text: "D3", size: 35 },
+        { text: "Cloud", size: 45 },
+        { text: "Visualization", size: 20 },
+        { text: "Frontend", size: 30 },
+        { text: "Next.js", size: 30 },
+        { text: "React", size: 50 },
+        { text: "JavaScript", size: 40 },
+        { text: "Web", size: 25 },
+        { text: "D3", size: 35 },
+        { text: "Cloud", size: 45 },
+        { text: "Visualization", size: 20 },
+        { text: "Frontend", size: 30 },
+    ]
     return (
         <div>
             <Header />
             <main className={styles.mainHome}>
-                <h1>WordCloud</h1>
-           
+                <section className={styles.sectionCloud}>
+                    <h1>WordCloud</h1>
+                    
+                    <input type="text" />
+                    <div className={styles.divCloud}>
+                        <D3Cloud words={words} />
+                    </div>
+                </section>
             </main>
             <Footer />
         </div>

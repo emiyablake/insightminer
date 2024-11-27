6ce6a9f5db5586964edab3405524ccdea24f02a7
emiyablake <maju.ads.02@gmail.com>
2024-11-26 21:44:07 -0300

    refactor user profile page

--- client/app/Components/HamburgerMenu/HamburgerMenu.tsx
+++ client/app/Components/HamburgerMenu/HamburgerMenu.tsx
@@ -1,15 +1,33 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Link from "next/link";
 import styles from "./HamburgerMenu.module.css"
 
 const HamburgerMenu: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+
+  const closeMenu = () => {
+    setMenuOpen(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        closeMenu();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <div className={styles.menuBurgue}>
@@ -18,10 +36,10 @@ const HamburgerMenu: React.FC = () => {
       </button>
       <nav className={`${styles.navBurgue} ${menuOpen ? styles.open : ""}`}>
         <div className={styles.styleNav}>
-          <button><Link href="/home">Home</Link></button>
-          <button> <Link href="/wordcloud">Nuvem de palavras</Link></button>
-          <button><Link href="/managerWordkey">Gerenciador de palavra chave</Link></button>
-          <button><Link href="/userProfile">Perfil do usuario</Link></button>
+          <button onClick={closeMenu} ><Link href="/home">Home</Link></button>
+          <button onClick={closeMenu}> <Link href="/wordcloud">Nuvem de palavras</Link></button>
+          <button onClick={closeMenu}><Link href="/managerWordkey">Gerenciador de palavra chave</Link></button>
+          <button onClick={closeMenu}><Link href="/userProfile">Perfil do usuario</Link></button>
         </div>
       </nav>
     </div>
